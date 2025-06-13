618eadd18ba66f8f2e4962a83ec25e1a2bfb297c
Anton <garynets.anton@gmail.com>
2025-06-13 13:30:16 +0300

    Added page for articles

--- news/urls.py
+++ news/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("article/<str:article_title>/<int:article_id>", views.article, name="article")
 ]
--- news/views.py
+++ news/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils.text import slugify
 from .models import *
 
 def index(request):
+    articles = Article.objects.all().values()
+
+    # add slug from the title to the dict
+    for item in articles:
+        item["slug"] = slugify(item["title"])
+
     context = {
-        "articles": Article.objects.all().values()
+        "articles": articles
     }
     return render(request, "news/index.html", context)
+
+def article(request, article_title, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+
+    context = {
+        "article": article
+    }
+
+    return redirect("article", slugify(article.title), article_id) if slugify(article.title) != article_title \
+        else render(request, "news/article.html", context)
