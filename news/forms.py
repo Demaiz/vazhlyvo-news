249cc3297b11c73687b5ea7da4c8ae8af61020d3
Anton <garynets.anton@gmail.com>
2025-07-16 13:30:11 +0300

    Refactored search page

--- news/forms.py
+++ news/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 class SearchForm(forms.Form):
-    search = forms.CharField(required=False, widget=forms.TextInput(attrs={"class":"form-control", "type":"text"}))
-    date_start = forms.DateField(required=False, widget=forms.DateInput(attrs={"class":"form-control", "type":"date"}))
-    date_stop = forms.DateField(required=False, widget=forms.DateInput(attrs={"class": "form-control", "type": "date"}))
+    search = forms.CharField(required=False, widget=forms.TextInput(attrs={"class":"form-control", "type":"text", "placeholder": _("Search")}))
+    date_start = forms.DateField(required=False, label=_("From"), widget=forms.DateInput(attrs={"class":"form-control", "type":"date"}))
+    date_stop = forms.DateField(required=False, label=_("To"), widget=forms.DateInput(attrs={"class": "form-control", "type": "date"}))
